a0d90e0f212e27d4882538ab1d170bb6b9bd190b
Rodrigo <rdg.hora1@gmail.com>
2023-10-24 10:28:25 -0300

    Lifecycle methods (métodos de ciclo de vida) - Parte 1

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 
 class App extends Component {
   state = {
+    counter: 0,
     posts: [
       {
         id: 1,
@@ -21,12 +22,38 @@ class App extends Component {
       },
     ],
   };
+  timeoutUpdate = null;
+
+  // Quando eu montar o componente inteiro na página eu disparo essa função
+  componentDidMount() {
+    this.handleTimeout();
+  }
+
+  // Quando o componente for atualizado eu disparo essa
+  componentDidUpdate() {
+    this.handleTimeout();
+  }
+
+  // Limpando o lixo da página quando o componente for ser desmontado
+  componentWillUnmount() {
+    clearTimeout(this.timeoutUpdate);
+  }
+
+  handleTimeout = () => {
+    const { posts, counter } = this.state;
+    posts[0].title = "O Título mudou";
+
+    this.timeoutUpdate = setTimeout(() => {
+      this.setState({ posts, counter: counter + 1 });
+    }, 1000);
+  };
 
   render() {
-    const { posts } = this.state;
+    const { posts, counter } = this.state;
 
     return (
       <div className="App">
+        <h1>{counter}</h1>
         {posts.map((post) => (
           <div key={post.id}>
             <h1>{post.title}</h1>
