aba46cac570b466f15a96db7867908668d09c3fb
Rodrigo <rdg.hora1@gmail.com>
2023-10-24 09:50:09 -0300

    Componentes de classe com estado (stateful)

--- src/App.js
+++ src/App.js
@@ -4,21 +4,36 @@ import "./App.css";
 import logo from "./logo.svg";
 
 class App extends Component {
+  state = { name: "Rodrigo Silva", counter: 0 };
+
+  handlePClick = () => {
+    this.setState({ name: "Junior" });
+  };
+
+  handleAClick = (event) => {
+    event.preventDefault();
+    const { counter } = this.state;
+    this.setState({ counter: counter + 1 });
+  };
+
   render() {
+    const { name, counter } = this.state;
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
+          <p onClick={this.handlePClick}>
+            {name} {counter}
           </p>
           <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
+            onClick={this.handleAClick}
           >
-            Learn React
+            Este é o LINK
           </a>
         </header>
       </div>
