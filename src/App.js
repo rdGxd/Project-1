b3ddda7c60c0448e0ea661f68265fbee5c7dda64
Rodrigo <rdg.hora1@gmail.com>
2023-10-24 11:18:15 -0300

    Buscando dados externos com fetch (Data fetching)

--- src/App.js
+++ src/App.js
@@ -3,64 +3,56 @@ import "./App.css";
 
 class App extends Component {
   state = {
-    counter: 0,
-    posts: [
-      {
-        id: 1,
-        title: "O título 1",
-        body: "O corpo 1",
-      },
-      {
-        id: 2,
-        title: "O título 2",
-        body: "O corpo 2",
-      },
-      {
-        id: 3,
-        title: "O título 3",
-        body: "O corpo 3",
-      },
-    ],
+    posts: [],
+    photos: [],
   };
-  timeoutUpdate = null;
 
   // componentDidMount() É invocado imediatamente após um componente ser montado (inserido na árvore). Inicializações que exijam nós do DOM devem vir aqui. Se precisar carregar data de um endpoint remoto, este é um bom lugar para instanciar sua requisição.
   componentDidMount() {
-    this.handleTimeout();
+    this.loadPosts();
   }
 
-  // componentDidUpdate() é invocado imediatamente após alguma atualização ocorrer. Este método não é chamado pelo initial render
-  componentDidUpdate() {
-    this.handleTimeout();
-  }
+  // Pegando os posts
+  loadPosts = async () => {
+    const postsResponse = fetch("https://jsonplaceholder.org/posts");
+    const photosResponse = fetch("https://jsonplaceholder.typicode.com/photos");
 
-  // componentWillUnmount() é invocado imediatamente antes que um componente seja desmontado e destruído. Qualquer limpeza necessária deve ser realizada neste método, como invalidar timers, cancelar requisições de rede, ou limpar qualquer subscrição que foi criada no componentDidMount().
-  componentWillUnmount() {
-    clearTimeout(this.timeoutUpdate);
-  }
+    const [posts, photos] = await Promise.all([postsResponse, photosResponse]);
 
-  handleTimeout = () => {
-    const { posts, counter } = this.state;
-    posts[0].title = "O Título mudou";
+    const postsJson = await posts.json();
+    const photosJson = await photos.json();
 
-    this.timeoutUpdate = setTimeout(() => {
-      this.setState({ posts, counter: counter + 1 });
-    }, 1000);
+    // Pegando uma foto por post
+    const postsAndPhotos = postsJson.map((post, index) => {
+      return { ...post, cover: photosJson[index].url };
+    });
+
+    this.setState({ posts: postsAndPhotos });
   };
 
+  // componentDidUpdate() é invocado imediatamente após alguma atualização ocorrer. Este método não é chamado pelo initial render
+  componentDidUpdate() {}
+
+  // componentWillUnmount() é invocado imediatamente antes que um componente seja desmontado e destruído. Qualquer limpeza necessária deve ser realizada neste método, como invalidar timers, cancelar requisições de rede, ou limpar qualquer subscrição que foi criada no componentDidMount().
+  componentWillUnmount() {}
+
   render() {
-    const { posts, counter } = this.state;
+    const { posts } = this.state;
 
     return (
-      <div className="App">
-        <h1>{counter}</h1>
-        {posts.map((post) => (
-          <div key={post.id}>
-            <h1>{post.title}</h1>
-            <p>{post.body}</p>
-          </div>
-        ))}
-      </div>
+      <section className="container">
+        <div className="posts">
+          {posts.map((post) => (
+            <div className="post">
+              <img src={post.cover} alt={post.title} />
+              <div key={post.id} className="post-content">
+                <h1>{post.title}</h1>
+                <p>{post.content}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
     );
   }
 }
