3e0a8fc071bcebe7f649d46e7b0521eaa5b651d9
Rodrigo <rdg.hora1@gmail.com>
2023-10-25 09:43:00 -0300

    Trabalhando com  inputs de texto

---
+++ src/components/TextInput/index.jsx
@@ -0,0 +1,12 @@
+import "./styles.css";
+export const TextInput = ({ searchValue, handleChange }) => {
+  return (
+    <input
+      onChange={handleChange}
+      type="search"
+      value={searchValue}
+      className="text-input"
+      placeholder="Type your search"
+    />
+  );
+};
--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -4,6 +4,7 @@ import "./styles.css";
 
 import { Button } from "../../components/Button";
 import { Posts } from "../../components/Posts";
+import { TextInput } from "../../components/TextInput";
 import { loadPosts } from "../../utils/load-posts";
 
 export class Home extends Component {
@@ -11,7 +12,8 @@ export class Home extends Component {
     posts: [],
     allPosts: [],
     page: 0,
-    postsPerPage: 10,
+    postsPerPage: 2,
+    searchValue: "",
   };
 
   // Pegando os posts e fotos
@@ -45,21 +47,51 @@ export class Home extends Component {
   // componentWillUnmount() é invocado imediatamente antes que um componente seja desmontado e destruído. Qualquer limpeza necessária deve ser realizada neste método, como invalidar timers, cancelar requisições de rede, ou limpar qualquer subscrição que foi criada no componentDidMount().
   componentWillUnmount() {}
 
+  // Pegando o valor do input
+  handleChange = (e) => {
+    const { value } = e.target;
+    this.setState({ searchValue: value });
+  };
+
   render() {
-    const { posts, page, postsPerPage, allPosts } = this.state;
+    const { posts, page, postsPerPage, allPosts, searchValue } = this.state;
     // Checando se tem mais posts
     const noMorePosts = page + postsPerPage >= allPosts.length;
 
+    // Se tiver valor no searchValue eu irei filtrar os posts retornado todos os posts que contem o searchValue se nao tiver valor irei retornar os posts normais
+    const filteredPosts = searchValue
+      ? allPosts.filter((post) => {
+          return post.title.toLowerCase().includes(searchValue.toLowerCase());
+        })
+      : posts;
+
     return (
       <section className="container">
-        <Posts posts={posts} />
+        <div className="search-container">
+          {/* Checando se existe um valor em searchValue */}
+          {searchValue && <h1>Search value: {searchValue}</h1>}
+          <TextInput
+            handleChange={this.handleChange}
+            searchValue={searchValue}
+          />
+        </div>
+
+        {/* Checando se existem posts depois de filtrar */}
+        {filteredPosts.length > 0 ? (
+          <Posts posts={filteredPosts} />
+        ) : (
+          <p>Não existem posts</p>
+        )}
 
         <div className="button-container">
-          <Button
-            text="Load more posts"
-            onClick={this.loadMorePosts}
-            disabled={noMorePosts}
-          />
+          {/* Se o searchValue for true o botão não será exibido */}
+          {!searchValue && (
+            <Button
+              text="Load more posts"
+              onClick={this.loadMorePosts}
+              disabled={noMorePosts}
+            />
+          )}
         </div>
       </section>
     );
