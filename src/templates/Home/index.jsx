42450768e3a818190bdc822ba9481277b86c76e1
Rodrigo Silva <seu_email@email.com>
2023-10-31 09:48:36 -0300

    Criando meu segundo hook - part 1

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,73 +1,45 @@
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
-import "./styles.css";
+const useFetch = (url, options) => {
+  const [result, setResult] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-import { Button } from "../../components/Button";
-import { Posts } from "../../components/Posts";
-import { TextInput } from "../../components/TextInput";
-import { loadPosts } from "../../utils/load-posts";
+  useEffect(() => {
+    console.log("EFFECT", new Date().toLocaleString());
+
+    // let wait = false;
+    setLoading(true);
+
+    const fetchData = async () => {
+      await new Promise((r) => setTimeout(r, 3000));
+
+      try {
+        const response = await fetch(url, options);
+        const jsonResult = await response.json();
+        setResult(jsonResult);
+        setLoading(false);
+      } catch (error) {
+        setLoading(false);
+        throw error;
+      }
+    };
+
+    fetchData();
+  }, [url, options]);
+
+  return [result, loading];
+};
 
 export const Home = () => {
-  const [posts, setPosts] = useState([]);
-  const [allPosts, setAllPosts] = useState([]);
-  const [page, setPage] = useState(0);
-  const [postsPerPage] = useState(3);
-  const [searchValue, setSearchValue] = useState("");
+  const [result, loading] = useFetch("https://jsonplaceholder.typicode.com/posts");
 
-  // Carregando os Posts
-  const handleLoadPosts = useCallback(async (page, postsPerPage) => {
-    const postsAndPhotos = await loadPosts();
-    setPosts(postsAndPhotos.slice(page, postsPerPage));
-    setAllPosts(postsAndPhotos);
-  }, []);
+  if (loading) {
+    return <p>Loading...</p>;
+  }
 
-  // Fazendo a paginação
-  const loadMorePosts = () => {
-    const nextPage = page + postsPerPage;
-    const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
-    posts.push(...nextPosts);
+  if (!loading && result) {
+    console.log(result);
+  }
 
-    setPosts(posts);
-    setPage(nextPage);
-  };
-
-  // Pegando o valor do input
-  const handleChange = (e) => {
-    const { value } = e.target;
-    setSearchValue(value);
-  };
-
-  // Se as dependências mudarem entre as renderizações, o useEffect executará a função de retorno de chamada
-  useEffect(() => {
-    // Função a ser executada
-    handleLoadPosts(0, postsPerPage);
-  }, [postsPerPage, handleLoadPosts]);
-
-  // Checando se tem mais posts
-  const noMorePosts = page + postsPerPage >= allPosts.length;
-
-  // Se tiver valor no searchValue eu irei filtrar os posts retornado todos os posts que contem o searchValue se nao tiver valor irei retornar os posts normais
-  const filteredPosts = searchValue
-    ? allPosts.filter((post) => {
-        return post.title.toLowerCase().includes(searchValue.toLowerCase());
-      })
-    : posts;
-
-  return (
-    <section className="container">
-      <div className="search-container">
-        {/* Checando se existe um valor em searchValue e exibindo ele */}
-        {searchValue && <h1>Search value: {searchValue}</h1>}
-        <TextInput handleChange={handleChange} searchValue={searchValue} />
-      </div>
-
-      {/* Checando se existem posts depois de filtrar */}
-      {filteredPosts.length > 0 ? <Posts posts={filteredPosts} /> : <p>Não existem posts</p>}
-
-      <div className="button-container">
-        {/* Se o searchValue for true o botão não será exibido */}
-        {!searchValue && <Button text="Load more posts" onClick={loadMorePosts} disabled={noMorePosts} />}
-      </div>
-    </section>
-  );
+  return <h1>Oi</h1>;
 };
