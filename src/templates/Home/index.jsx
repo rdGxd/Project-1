ab3d46e625c3ce453bb832f72b5c944b7a784d1b
Rodrigo Silva <seu_email@email.com>
2023-10-31 11:43:14 -0300

    useAsync

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,39 +1,74 @@
-import { useState } from "react";
-import { useFetch } from "./use-fetch";
+import { useCallback, useEffect, useState } from "react";
 
-export const Home = () => {
-  const [postId, setPostId] = useState("");
-  const [result, loading] = useFetch("https://jsonplaceholder.typicode.com/posts/" + postId, {
-    headers: {
-      abc: "1" + postId,
-    },
+const useAsync = (asyncFunction, shouldRun) => {
+  const [state, setState] = useState({
+    result: null,
+    error: null,
+    status: "idle",
   });
 
-  if (loading) {
-    return <p>Loading...</p>;
-  }
+  const run = useCallback(() => {
+    setState({
+      result: null,
+      error: null,
+      status: "pending",
+    });
 
-  const handleClick = (id) => {
-    setPostId(id);
+    return asyncFunction()
+      .then((response) => {
+        setState({
+          result: response,
+          error: null,
+          status: "settled",
+        });
+      })
+      .catch((error) => {
+        setState({
+          result: null,
+          error: error,
+          status: "error",
+        });
+      });
+  }, [asyncFunction]);
+
+  useEffect(() => {
+    if (shouldRun) {
+      run();
+    }
+  }, [run, shouldRun]);
+
+  return [run, state.result, state.error, state.status];
+};
+
+const fetchData = async () => {
+  const data = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const json = await data.json();
+  return json;
+};
+
+export const Home = () => {
+  const [reFetchData, result, error, status] = useAsync(fetchData, true);
+
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     reFetchData();
+  //   }, 5000);
+  // }, [reFetchData]);
+
+  const handleClick = () => {
+    reFetchData();
   };
 
-  if (!loading && result) {
-    return (
-      <div>
-        {result?.length > 0 ? (
-          result.map((post) => (
-            <div key={`post-${post.id}`} onClick={() => handleClick(post.id)}>
-              <p>{post.title}</p>
-            </div>
-          ))
-        ) : (
-          <div onClick={() => handleClick("")}>
-            <p>{result.title}</p>
-          </div>
-        )}
-      </div>
-    );
+  if (status === "idle") {
+    return <pre>idle: Nada executando</pre>;
   }
-
-  return <h1>Oi</h1>;
+  if (status === "pending") {
+    return <pre>pending: Loading...</pre>;
+  }
+  if (status === "error") {
+    return <pre>error: {error.message}</pre>;
+  }
+  if (status === "settled") {
+    return <pre onClick={handleClick}>settled: {JSON.stringify(result, null, 2)}</pre>;
+  }
 };
