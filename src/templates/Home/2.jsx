0e914cf28946d288b9dbca498666be62a3a6968a
Rodrigo Silva <seu_email@email.com>
2023-11-01 06:41:39 -0300

    useLayoutEffect

---
+++ src/templates/Home/2.jsx
@@ -0,0 +1,68 @@
+import { useCallback, useEffect, useState } from "react";
+
+const useAsync = (asyncFunction, shouldRun) => {
+  const [state, setState] = useState({
+    result: null,
+    error: null,
+    status: "idle",
+  });
+
+  const run = useCallback(() => {
+    setState({
+      result: null,
+      error: null,
+      status: "pending",
+    });
+
+    return asyncFunction()
+      .then((response) => {
+        setState({
+          result: response,
+          error: null,
+          status: "settled",
+        });
+      })
+      .catch((error) => {
+        setState({
+          result: null,
+          error: error,
+          status: "error",
+        });
+      });
+  }, [asyncFunction]);
+
+  useEffect(() => {
+    if (shouldRun) {
+      run();
+    }
+  }, [run, shouldRun]);
+
+  return [run, state.result, state.error, state.status];
+};
+
+const fetchData = async () => {
+  const data = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const json = await data.json();
+  return json;
+};
+
+export const Home = () => {
+  const [reFetchData, result, error, status] = useAsync(fetchData, true);
+
+  const handleClick = () => {
+    reFetchData();
+  };
+
+  if (status === "idle") {
+    return <pre>idle: Nada executando</pre>;
+  }
+  if (status === "pending") {
+    return <pre>pending: Loading...</pre>;
+  }
+  if (status === "error") {
+    return <pre>error: {error.message}</pre>;
+  }
+  if (status === "settled") {
+    return <pre onClick={handleClick}>settled: {JSON.stringify(result, null, 2)}</pre>;
+  }
+};
--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,74 +1,26 @@
-import { useCallback, useEffect, useState } from "react";
-
-const useAsync = (asyncFunction, shouldRun) => {
-  const [state, setState] = useState({
-    result: null,
-    error: null,
-    status: "idle",
-  });
-
-  const run = useCallback(() => {
-    setState({
-      result: null,
-      error: null,
-      status: "pending",
-    });
-
-    return asyncFunction()
-      .then((response) => {
-        setState({
-          result: response,
-          error: null,
-          status: "settled",
-        });
-      })
-      .catch((error) => {
-        setState({
-          result: null,
-          error: error,
-          status: "error",
-        });
-      });
-  }, [asyncFunction]);
-
-  useEffect(() => {
-    if (shouldRun) {
-      run();
-    }
-  }, [run, shouldRun]);
-
-  return [run, state.result, state.error, state.status];
-};
-
-const fetchData = async () => {
-  const data = await fetch("https://jsonplaceholder.typicode.com/posts");
-  const json = await data.json();
-  return json;
-};
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
 
 export const Home = () => {
-  const [reFetchData, result, error, status] = useAsync(fetchData, true);
+  const [counted, setCounted] = useState([0, 1, 2, 3, 4]);
+  const divRef = useRef();
 
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     reFetchData();
-  //   }, 5000);
-  // }, [reFetchData]);
+  // Sempre tente usar o useEffect no lugar
+  useLayoutEffect(() => {
+    divRef.current.scrollTop = divRef.current.scrollHeight;
+  });
 
   const handleClick = () => {
-    reFetchData();
+    setCounted((c) => [...c, Number(c.slice(-1)) + 1]);
   };
 
-  if (status === "idle") {
-    return <pre>idle: Nada executando</pre>;
-  }
-  if (status === "pending") {
-    return <pre>pending: Loading...</pre>;
-  }
-  if (status === "error") {
-    return <pre>error: {error.message}</pre>;
-  }
-  if (status === "settled") {
-    return <pre onClick={handleClick}>settled: {JSON.stringify(result, null, 2)}</pre>;
-  }
+  return (
+    <>
+      <button onClick={handleClick}>Count {counted.slice(-1)}</button>
+      <div ref={divRef} style={{ height: "100px", width: "100px", overflowY: "scroll" }}>
+        {counted.map((c) => {
+          return <p key={`c-${c}`}>{c}</p>;
+        })}
+      </div>
+    </>
+  );
 };
